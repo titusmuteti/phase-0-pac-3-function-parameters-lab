e3e05b10986703b1122bf0499c9ab3a4ddf209b6
Titus Mumo <titusmumo@gmail.com>
2022-08-08 23:12:05 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction (name = "Aki") {
+    return `Hi, my name is ${name}.`
+}
+function introductionWithLanguage(name = "Aki", language = "Ember Js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
